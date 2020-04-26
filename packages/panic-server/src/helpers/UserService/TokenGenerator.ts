9444d1e2c7de368e167ae07de04c9a64fbb03573
ioanardelean <ioana.ardelean6@gmail.com>
2020-04-26 15:51:43 +0200

    wip: jenkins integration

--- packages/panic-server/src/helpers/UserService/TokenGenerator.ts
+++ packages/panic-server/src/helpers/UserService/TokenGenerator.ts
@@ -2,7 +2,7 @@ import config from 'config';
 import fs from 'fs';
 import jwt from 'jsonwebtoken';
 const cwd = process.cwd();
-const PRIV_KEY = fs.readFileSync(`${cwd}/src/private.key`, 'utf8');
+const PRIV_KEY = fs.readFileSync(`${cwd}/private.key`, 'utf8');
 const configJwt: any = config.get('tokenLife');
 export async function JwtSign(payload: any) {
   return jwt.sign({ ...payload }, PRIV_KEY || 'secret', {
