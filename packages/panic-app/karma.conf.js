4c764e16f3b048bedf4455418a024eed16d1b759
ioanardelean <ioana.ardelean6@gmail.com>
2020-04-28 22:45:42 +0200

    wip: fix karma config

--- packages/panic-app/karma.conf.js
+++ packages/panic-app/karma.conf.js
@@ -25,8 +25,9 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
     singleRun: true,
     restartOnFileChange: true,
+    concurrency: Infinity,
+    browsers: ['ChromeHeadless'],
   });
 };
