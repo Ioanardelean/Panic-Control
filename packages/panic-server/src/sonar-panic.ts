ec6ac26dc096ee04cb367622249d456d710310c2
ioanardelean <ioana.ardelean6@gmail.com>
2020-07-08 15:52:29 +0200

    wip: test duplication

--- packages/panic-server/src/sonar-panic.ts
+++ packages/panic-server/src/sonar-panic.ts
@@ -1,10 +1,12 @@
-const sonarqubeScanner = require('sonarqube-scanner');
+import sonarqubeScanner from 'sonarqube-scanner';
+
 sonarqubeScanner(
   {
     serverUrl: 'http://localhost:9000',
     options: {
       'sonar.sources': '.',
       'sonar.inclusions': 'src/**', // Entry point of your code
+      'sonar.exclusions': '***/*.spec.ts',
     },
   },
   () => {}
--- packages/panic-server/src/test/integration/history.spec.ts
+++ packages/panic-server/src/test/integration/history.spec.ts
@@ -1,13 +1,14 @@
 import request from 'supertest';
 import app from '../../app';
 let token: any = null;
+const contentType = 'application/json';
 
-beforeEach(async (done) => {
+beforeEach(async done => {
   await request(app.callback())
     .post('/auth/login')
-    .send({ username: 'test', password: 'Passtest123' })
-    .set('Accept', 'application/json')
-    .expect((res) => {
+    .send({ username: 'test', password: process.env.USER_PASSWORD_TEST })
+    .set('Accept', contentType)
+    .expect(res => {
       token = res.body.token;
       done();
     });
@@ -15,10 +16,20 @@ beforeEach(async (done) => {
 let id: any = null;
 
 describe('POST /projects creation', () => {
-  it('respond with 200 new project created', async (done) => {
-    await request(app.callback()).post('/projects/').set('Authorization', `Bearer ${token}`).set('Accept', 'application/json')
-      .send({ name: 'test', url: 'http://test2.com', receiver: 'test@test2.com', ping: 10, monitorInterval: 1, testRunning: true })
-      .expect((res) => {
+  it('respond with 200 new project created', async done => {
+    await request(app.callback())
+      .post('/projects/')
+      .set('Authorization', `Bearer ${token}`)
+      .set('Accept', contentType)
+      .send({
+        name: 'test',
+        url: 'http://test2.com',
+        receiver: 'test@test2.com',
+        ping: 10,
+        monitorInterval: 1,
+        testRunning: true,
+      })
+      .expect(res => {
         id = res.body.data.id;
 
         done();
@@ -28,29 +39,40 @@ describe('POST /projects creation', () => {
 
 describe('GET /history stats', () => {
   it('respond with 200 get last static', async () => {
-    await request(app.callback()).get('/history/last').set('Authorization', `Bearer ${token}`).set('Accept', 'application/json')
+    await request(app.callback())
+      .get('/history/last')
+      .set('Authorization', `Bearer ${token}`)
+      .set('Accept', contentType)
       .expect(200);
   });
 });
 
 describe('GET /history stats on year', () => {
   it('respond with 200 all on year', async () => {
-    await request(app.callback()).get(`/history/${id}/downtime-year`).set('Authorization', `Bearer ${token}`).set('Accept', 'application/json')
+    await request(app.callback())
+      .get(`/history/${id}/downtime-year`)
+      .set('Authorization', `Bearer ${token}`)
+      .set('Accept', contentType)
       .expect(200);
   });
 });
 
-
 describe('GET /history stats on month', () => {
   it('respond with 200 all on month', async () => {
-    await request(app.callback()).get(`/history/downtime-month`).set('Authorization', `Bearer ${token}`).set('Accept', 'application/json')
+    await request(app.callback())
+      .get(`/history/downtime-month`)
+      .set('Authorization', `Bearer ${token}`)
+      .set('Accept', contentType)
       .expect(200);
   });
 });
 
 describe('GET /history stats since creation', () => {
   it('respond with 200 all since creation', async () => {
-    await request(app.callback()).get(`/history/${id}/downtime`).set('Authorization', `Bearer ${token}`).set('Accept', 'application/json')
+    await request(app.callback())
+      .get(`/history/${id}/downtime`)
+      .set('Authorization', `Bearer ${token}`)
+      .set('Accept', contentType)
       .expect(200);
   });
 });
